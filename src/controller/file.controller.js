810819808c560c681e2c6c43194c85662d0fa8d3
Muntasim <muntasim.ahmed@autodesk.com>
2021-05-08 11:31:36 +0800

    init commit

--- src/controller/file.controller.js
+++ src/controller/file.controller.js
@@ -1,35 +1,8 @@
-const uploadFile = require("../middleware/upload");
 const fs = require("fs");
 const baseUrl = "http://localhost:8080/files/";
 
-const upload = async (req, res) => {
-  try {
-    await uploadFile(req, res);
-
-    if (req.file == undefined) {
-      return res.status(400).send({ message: "Please upload a file!" });
-    }
-
-    res.status(200).send({
-      message: "Uploaded the file successfully: " + req.file.originalname,
-    });
-  } catch (err) {
-    console.log(err);
-
-    if (err.code == "LIMIT_FILE_SIZE") {
-      return res.status(500).send({
-        message: "File size cannot be larger than 2MB!",
-      });
-    }
-
-    res.status(500).send({
-      message: `Could not upload the file: ${req.file.originalname}. ${err}`,
-    });
-  }
-};
-
 const getListFiles = (req, res) => {
-  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  const directoryPath = __basedir + "/resources/static/assets/";
 
   fs.readdir(directoryPath, function (err, files) {
     if (err) {
@@ -53,7 +26,7 @@ const getListFiles = (req, res) => {
 
 const download = (req, res) => {
   const fileName = req.params.name;
-  const directoryPath = __basedir + "/resources/static/assets/uploads/";
+  const directoryPath = __basedir + "/resources/static/assets/";
 
   res.download(directoryPath + fileName, fileName, (err) => {
     if (err) {
@@ -65,7 +38,6 @@ const download = (req, res) => {
 };
 
 module.exports = {
-  upload,
   getListFiles,
   download,
 };
